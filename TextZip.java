a8b64fe576c1f9ebbd8ab55a659bf44c5cd76287
Hao Chen <ilff8chen@gmail.com>
2014-05-27 17:46:04 +1200

    Completed task 1

--- TextZip.java
+++ TextZip.java
@@ -2,20 +2,20 @@ import java.io.*;
 import java.util.*;
 
 /**
-	COMPSCI 105 S1 C, 2014
-	Assignment Three Question 1
+COMPSCI 105 S1 C, 2014
+Assignment Three Question 1
 
-	@author  	YOUR NAME and UPI
-	@version	THE DATE
+@author  	YOUR NAME and UPI
+@version	THE DATE
 **/
 
 public class TextZip {
-
-  /**
-	* This method generates the huffman tree for the text: "abracadabra!"
-	*
-	* @return the root of the huffman tree
-	*/
+	static boolean debug = false;
+	/**
+		* This method generates the huffman tree for the text: "abracadabra!"
+		*
+		* @return the root of the huffman tree
+		*/
 	public static TreeNode<CharFreq> abracadbraTree() {
 		TreeNode<CharFreq> n0 = new TreeNode<CharFreq>(new CharFreq('!', 1));
 		TreeNode<CharFreq> n1 = new TreeNode<CharFreq>(new CharFreq('c', 1));
@@ -31,93 +31,98 @@ public class TextZip {
 		return n10;
 	}
 
-  /**
-	* This method decompresses a huffman compressed text file.  The compressed
-	* file must be read one bit at a time using the supplied BitReader, and
-	* then by traversing the supplied huffman tree, each sequence of compressed
-	* bits should be converted to their corresponding characters.  The
-	* decompressed characters should be written to the FileWriter
-	*
-	* @param  br:      the BitReader which reads one bit at a time from the
-	*                  compressed file
-	*         huffman: the huffman tree that was used for compression, and
-	*                  hence should be used for decompression
-	*         fw:      a FileWriter for storing the decompressed text file
-	*/
+	/**
+		* This method decompresses a huffman compressed text file.  The compressed
+		* file must be read one bit at a time using the supplied BitReader, and
+		* then by traversing the supplied huffman tree, each sequence of compressed
+		* bits should be converted to their corresponding characters.  The
+		* decompressed characters should be written to the FileWriter
+		*
+		* @param  br:      the BitReader which reads one bit at a time from the
+		*                  compressed file
+		*         huffman: the huffman tree that was used for compression, and
+		*                  hence should be used for decompression
+		*         fw:      a FileWriter for storing the decompressed text file
+		*/
 	public static void decompress(BitReader br, TreeNode<CharFreq> huffman, FileWriter fw) throws Exception {
 
 		// IMPLEMENT THIS METHOD
+		TreeNode<CharFreq> currentNode;
+		currentNode = huffman;
 		String val = "";
 		while(br.hasNext()){
 			if(br.next()){
-				System.out.print("1");
-				val += 1;
+				if(debug)
+					System.out.print("1");
+				if(!currentNode.isLeaf()){
+					currentNode = currentNode.getRight();
+					if(currentNode.isLeaf()){
+						val += currentNode.getItem().getChar();
+						currentNode = huffman;
+					}
+				}
 			}
 			else {
-				System.out.print("0");
-				val += 0;
+				if(debug)
+					System.out.print("0");
+				if(!currentNode.isLeaf()){
+					currentNode = currentNode.getLeft();
+					if(currentNode.isLeaf()){
+						val += currentNode.getItem().getChar();
+						currentNode = huffman;
+					}
+				}
 			}
-		}		
-		System.out.println("\n" + val);
-		
-		String s = "";
-		for(int i = 0; i < val.length(); i++){
-			s = val.substring(i,i+1);
-			int currentVal = Integer.parseInt(s);
-			System.out.println(currentVal);
 		}
-
-
-		System.out.println(huffman.getLeft().getChar());
-		System.out.println("Here2");
-		
-		fw.write("aasdasd");
+		if(debug)		
+			System.out.println("\n" + val);	
+		fw.write(val);
 		
 
 	}
 
-  /**
-	* This method traverses the supplied huffman tree and prints out the
-	* codes associated with each character
-	*
-	* @param  t:    the root of the huffman tree to be traversed
-	*         code: a String used to build the code for each character as
-	*               the tree is traversed recursively
-	*/
+	/**
+		* This method traverses the supplied huffman tree and prints out the
+		* codes associated with each character
+		*
+		* @param  t:    the root of the huffman tree to be traversed
+		*         code: a String used to build the code for each character as
+		*               the tree is traversed recursively
+		*/
 	public static void traverse(TreeNode<CharFreq> t, String code) {
 
 		// IMPLEMENT THIS METHOD
 		
 	}
 
- /**
-	* This method traverses the supplied huffman tree and stores the codes
-	* associated with each character in the supplied array.
-	*
-	* @param  t:     the root of the huffman tree to be traversed
-	*         code:  a String used to build the code for each character as
-	*                the tree is traversed recursively
-	*         codes: an array to store the code for each character.  The indexes
-	*                of this array range from 0 to 127 and represent the ASCII
-	*                value of the characters.
-	*/
+	/**
+		* This method traverses the supplied huffman tree and stores the codes
+		* associated with each character in the supplied array.
+		*
+		* @param  t:     the root of the huffman tree to be traversed
+		*         code:  a String used to build the code for each character as
+		*                the tree is traversed recursively
+		*         codes: an array to store the code for each character.  The indexes
+		*                of this array range from 0 to 127 and represent the ASCII
+		*                value of the characters.
+		*/
 	public static void traverse(TreeNode<CharFreq> t, String code, String[] codes) {
 		
 		// IMPLEMENT THIS METHOD
 
 	}
 
-  /**
-	* This method removes the TreeNode, from an ArrayList of TreeNodes,  which
-	* contains the smallest item.  The items stored in each TreeNode must
-	* implement the Comparable interface.
-	* The ArrayList must contain at least one element.
-	*
-	* @param  a: an ArrayList containing TreeNode objects
-	*
-	* @return the TreeNode in the ArrayList which contains the smallest item.
-	*         This TreeNode is removed from the ArrayList.
-	*/
+	/**
+		* This method removes the TreeNode, from an ArrayList of TreeNodes,  which
+		* contains the smallest item.  The items stored in each TreeNode must
+		* implement the Comparable interface.
+		* The ArrayList must contain at least one element.
+		*
+		* @param  a: an ArrayList containing TreeNode objects
+		*
+		* @return the TreeNode in the ArrayList which contains the smallest item.
+		*         This TreeNode is removed from the ArrayList.
+		*/
 	public static TreeNode<CharFreq> removeMin(ArrayList<TreeNode<CharFreq>> a) {
 		int minIndex = 0;
 		for (int i = 0; i < a.size(); i++) {
@@ -130,102 +135,102 @@ public class TextZip {
 		return n;
 	}
 
-  /**
-	* This method counts the frequencies of each character in the supplied
-	* FileReader, and produces an output text file which lists (on each line)
-	* each character followed by the frequency count of that character.  This
-	* method also returns an ArrayList which contains TreeNodes.  The item stored
-	* in each TreeNode in the returned ArrayList is a CharFreq object, which
-	* stores a character and its corresponding frequency
-	*
-	* @param  fr: the FileReader for which the character frequencies are being
-	*             counted
-	*         pw: the PrintWriter which is used to produce the output text file
-	*             listing the character frequencies
-	*
-	* @return the ArrayList containing TreeNodes.  The item stored in each
-	*         TreeNode is a CharFreq object.
-	*/
+	/**
+		* This method counts the frequencies of each character in the supplied
+		* FileReader, and produces an output text file which lists (on each line)
+		* each character followed by the frequency count of that character.  This
+		* method also returns an ArrayList which contains TreeNodes.  The item stored
+		* in each TreeNode in the returned ArrayList is a CharFreq object, which
+		* stores a character and its corresponding frequency
+		*
+		* @param  fr: the FileReader for which the character frequencies are being
+		*             counted
+		*         pw: the PrintWriter which is used to produce the output text file
+		*             listing the character frequencies
+		*
+		* @return the ArrayList containing TreeNodes.  The item stored in each
+		*         TreeNode is a CharFreq object.
+		*/
 	public static ArrayList<TreeNode<CharFreq>> countFrequencies(FileReader fr, PrintWriter pw) throws Exception {
 
 		// IMPLEMENT THIS METHOD
 		return null;
 	}
 
-  /**
-	* This method builds a huffman tree from the supplied ArrayList of TreeNodes.
-	* Initially, the items in each TreeNode in the ArrayList store a CharFreq object.
-	* As the tree is built, the smallest two items in the ArrayList are removed,
-	* merged to form a tree with a CharFreq object storing the sum of the frequencies
-	* as the root, and the two original CharFreq objects as the children.  The right
-	* child must be the second of the two elements removed from the ArrayList (where
-	* the ArrayList is scanned from left to right when the minimum element is found).
-	* When the ArrayList contains just one element, this will be the root of the
-	* completed huffman tree.
-	*
-	* @param  trees: the ArrayList containing the TreeNodes used in the algorithm
-	*                for generating the huffman tree
-	*
-	* @return the TreeNode referring to the root of the completed huffman tree
-	*/
+	/**
+		* This method builds a huffman tree from the supplied ArrayList of TreeNodes.
+		* Initially, the items in each TreeNode in the ArrayList store a CharFreq object.
+		* As the tree is built, the smallest two items in the ArrayList are removed,
+		* merged to form a tree with a CharFreq object storing the sum of the frequencies
+		* as the root, and the two original CharFreq objects as the children.  The right
+		* child must be the second of the two elements removed from the ArrayList (where
+		* the ArrayList is scanned from left to right when the minimum element is found).
+		* When the ArrayList contains just one element, this will be the root of the
+		* completed huffman tree.
+		*
+		* @param  trees: the ArrayList containing the TreeNodes used in the algorithm
+		*                for generating the huffman tree
+		*
+		* @return the TreeNode referring to the root of the completed huffman tree
+		*/
 	public static TreeNode<CharFreq> buildTree(ArrayList<TreeNode<CharFreq>> trees) throws IOException {
 
 		// IMPLEMENT THIS METHOD
 		return null;
 	}
 
-  /**
-	* This method compresses a text file using huffman encoding.  Initially, the
-	* supplied huffman tree is traversed to generate a lookup table of codes for
-	* each character.  The text file is then read one character at a time, and
-	* each character is encoded by using the lookup table.  The encoded bits for
-	* each character are written one at a time to the specified BitWriter.
-	*
-	* @param  fr:      the FileReader which contains the text file to be encoded
-	*         huffman: the huffman tree that was used for compression, and
-	*                  hence should be used for decompression
-	*         bw:      the BitWriter used to write the compressed bits to file
-	*/
+	/**
+		* This method compresses a text file using huffman encoding.  Initially, the
+		* supplied huffman tree is traversed to generate a lookup table of codes for
+		* each character.  The text file is then read one character at a time, and
+		* each character is encoded by using the lookup table.  The encoded bits for
+		* each character are written one at a time to the specified BitWriter.
+		*
+		* @param  fr:      the FileReader which contains the text file to be encoded
+		*         huffman: the huffman tree that was used for compression, and
+		*                  hence should be used for decompression
+		*         bw:      the BitWriter used to write the compressed bits to file
+		*/
 	public static void compress(FileReader fr, TreeNode<CharFreq> huffman, BitWriter bw) throws Exception {
 
 		// IMPLEMENT THIS METHOD
 
 	}
 
-  /**
-	* This method reads a frequency file (such as those generated by the
-	* countFrequencies() method) and initialises an ArrayList of TreeNodes
-	* where the item of each TreeNode is a CharFreq object storing a character
-	* from the frequency file and its corresponding frequency.  This method provides
-	* the same functionality as the countFrequencies() method, but takes in a
-	* frequency file as parameter rather than a text file.
-	*
-	* @param  inputFreqFile: the frequency file which stores characters and their
-	*                        frequency (one character per line)
-	*
-	* @return the ArrayList containing TreeNodes.  The item stored in each
-	*         TreeNode is a CharFreq object.
-	*/
+	/**
+		* This method reads a frequency file (such as those generated by the
+		* countFrequencies() method) and initialises an ArrayList of TreeNodes
+		* where the item of each TreeNode is a CharFreq object storing a character
+		* from the frequency file and its corresponding frequency.  This method provides
+		* the same functionality as the countFrequencies() method, but takes in a
+		* frequency file as parameter rather than a text file.
+		*
+		* @param  inputFreqFile: the frequency file which stores characters and their
+		*                        frequency (one character per line)
+		*
+		* @return the ArrayList containing TreeNodes.  The item stored in each
+		*         TreeNode is a CharFreq object.
+		*/
 	public static ArrayList<TreeNode<CharFreq>> readFrequencies(String inputFreqFile) throws Exception {
 
 		// IMPLEMENT THIS METHOD
 		return null;
 	}
 	
-  /**
-	* This method prints out the sizes (in bytes) of the compressed and the 
-	* original files, and computes and prints out the compressed ratio.
-	*
-	* @param  file1: full name of the first file
-	*         file2: full name of the second file
-	*/	
+	/**
+		* This method prints out the sizes (in bytes) of the compressed and the 
+		* original files, and computes and prints out the compressed ratio.
+		*
+		* @param  file1: full name of the first file
+		*         file2: full name of the second file
+		*/	
 	public static void statistics(String file1, String file2) {
 		File compressedFile =new File(file1);
 		File originalFile =new File(file2);
 		System.out.println(file1 + " decompressed by hche608");
 		System.out.println("Size of the compressed file: " + compressedFile.length() + " bytes");
 		System.out.println("Size of the original file: " + originalFile.length() + " bytes");
-		System.out.println("Compressed ratio: " + (compressedFile.length() * 100 / originalFile.length()) + "%");
+		System.out.println("Compressed ratio: " + (compressedFile.length() * 100.0 / originalFile.length()) + "%");
 		
 	}
 	
@@ -234,41 +239,41 @@ public class TextZip {
 
 	QUESTION 1 PART 1
 	=================
-		 -a : this uses a default prefix code tree and its compressed
-		      file, "a.txz", and decompresses the file, storing the output
-		      in the text file, "a.txt".  It should also print out the size
-		      of the compressed file (in bytes), the size of the decompressed
-		      file (in bytes) and the compression ratio
+	-a : this uses a default prefix code tree and its compressed
+	file, "a.txz", and decompresses the file, storing the output
+	in the text file, "a.txt".  It should also print out the size
+	of the compressed file (in bytes), the size of the decompressed
+	file (in bytes) and the compression ratio
 
 	QUESTION 1 PART 2
 	=================
-		 -f : given a text file (args[1]) and the name of an output frequency file
-		      (args[2]) this should count the character frequencies in the text file
-		      and store these in the frequency file (with one character and its
-		      frequency per line).  It should then build the huffman tree based on
-		      the character frequencies, and then print out the prefix code for each
-		      character
+	-f : given a text file (args[1]) and the name of an output frequency file
+	(args[2]) this should count the character frequencies in the text file
+	and store these in the frequency file (with one character and its
+	frequency per line).  It should then build the huffman tree based on
+	the character frequencies, and then print out the prefix code for each
+	character
 
 	QUESTION 1 PART 3
 	=================
-		 -c : given a text file (args[1]) and the name of the output compressed 
-		      file (args[2]) and the name of an output frequency file (args[3]), 
-		      this should compress the file
+	-c : given a text file (args[1]) and the name of the output compressed 
+	file (args[2]) and the name of an output frequency file (args[3]), 
+	this should compress the file
 
 	QUESTION 1 PART 4
 	=================
-		 -d : given a compressed file (args[1]) and its corresponding frequency file
-		      (args[2]) and the name of the output decompressed text file (args[3]),
-		      this should decompress the file
+	-d : given a compressed file (args[1]) and its corresponding frequency file
+	(args[2]) and the name of the output decompressed text file (args[3]),
+	this should decompress the file
 
 	*/
 
 	public static void main(String[] args) throws Exception {
 
 		/* This is a standard sample command line implementation. If you choose to 
-		   implement your program with a Graphical User Interface (GUI), please 
-		   write your own implementation accordingly.
-		*/
+			implement your program with a Graphical User Interface (GUI), please 
+			write your own implementation accordingly.
+			*/
 		
 		if (args[0].equals("-a")) {
 			BitReader br = new BitReader("a.txz");
